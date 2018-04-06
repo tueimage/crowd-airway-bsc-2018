9241bd36078077f89ced90f82e8ed71b814ba28c
vcheplygina <vcheplygina@users.noreply.github.com>
2018-04-06 11:30:43 +0200

    Added comments and JSON parser

--- matlab/measureEllipses.m
+++ matlab/measureEllipses.m
@@ -1,40 +1,36 @@
-function [centerX, centerY, angle, radius1, radius2] = measureEllipses(annotations)   
-         
-      numAnnot=length(annotations);
-      
+function [centerX, centerY, angle, radius1, radius2] = measureEllipses(annotations)
+
+numAnnot=length(annotations);
+
 centerX = nan(numAnnot,1);
-            centerY = nan(numAnnot,1);
-            angle = nan(numAnnot,1);
-            radius1 = nan(numAnnot,1);
-            radius2 = nan(numAnnot,1);
-            
-        for k=1:length(annotations)
+centerY = nan(numAnnot,1);
+angle = nan(numAnnot,1);
+radius1 = nan(numAnnot,1);
+radius2 = nan(numAnnot,1);
 
-                %Draw ellipse
-                currentAnnotation = annotations(k);
-                currentAnnotation = currentAnnotation{1}; %due to parse_json
-                
-                pts=cell2mat(currentAnnotation.points{1});
-                ptsX = pts(1:2:end);
-                ptsY = pts(2:2:end);
-                  
-                centerX(k) = ptsX(1);
-                centerY(k) = ptsY(1);
-
-                v1 = [ptsX(1)-ptsX(2), ptsY(1)-ptsY(2)];
-                v2 = [1, 0];
-                dot = v1(1)*v2(1) + v1(2)*v2(2) ;    
-                det = v1(1)*v2(2) - v1(2)*v2(1) ;   
-                angle(k) = -atan2(det, dot);
-                    
-                vec1 = [centerX(k) centerY(k); ptsX(2) ptsY(2)];
-                vec2= [centerX(k) centerY(k); ptsX(3) ptsY(3)];
-                    
-                radius1(k) = pdist(vec1, 'euclidean');
-                radius2(k) = pdist(vec2, 'euclidean');
-                   
-                %ellipse(radius1(k),radius2(k),angle(k),centerX(k),centerY(k));    
-                
-                 
-                 
-            end
+for k=1:length(annotations)
+    
+    %Draw ellipse
+    currentAnnotation = annotations(k);
+    currentAnnotation = currentAnnotation{1}; %due to parse_json
+    
+    pts=cell2mat(currentAnnotation.points{1});
+    ptsX = pts(1:2:end);
+    ptsY = pts(2:2:end);
+    
+    centerX(k) = ptsX(1);
+    centerY(k) = ptsY(1);
+    
+    v1 = [ptsX(1)-ptsX(2), ptsY(1)-ptsY(2)];
+    v2 = [1, 0];
+    dot = v1(1)*v2(1) + v1(2)*v2(2) ;
+    det = v1(1)*v2(2) - v1(2)*v2(1) ;
+    angle(k) = -atan2(det, dot);
+    
+    vec1 = [centerX(k) centerY(k); ptsX(2) ptsY(2)];
+    vec2= [centerX(k) centerY(k); ptsX(3) ptsY(3)];
+    
+    radius1(k) = pdist(vec1, 'euclidean');
+    radius2(k) = pdist(vec2, 'euclidean');
+   
+end
--- matlab/viewAndProcessResults.m
+++ matlab/viewAndProcessResults.m
@@ -1,144 +1,127 @@
-%Load GT
-%load('C:\Users\Veronika\Dropbox\Postdoc\Lungs\luvar\LUVAR_i64_I3_o64_O1_d20_th70_5mm_tapering80_TMI_PAPER','data');
-%gt = data;
+%% Define data paths 
 
-dataPath = 'C:\Users\VCheplyg\Dropbox\10. Work\CrowdAirway\data\';
+user='Veronika';
 
-resultPath = 'C:\Users\VCheplyg\Dropbox\10. Work\CrowdAirway\results\';
+if strcmp(user,'Veronika')
+    dataPath = 'C:\Users\VCheplyg\Dropbox\10. Work\CrowdAirway\shared\data\';
+    resultPath = 'C:\Users\VCheplyg\Dropbox\10. Work\CrowdAirway\shared\results\';
+end
 
-%%
-%Load ground truth measurements (structure Adria)
+%% Load ground truth measurements 
 load(fullfile(dataPath, 'LUVAR_GoLive2.mat'),'data'); 
- gt=data;
- clear data;
- 
- addpath('C:\Users\VCheplyg\Dropbox\MATLAB\json\parse_json_2009_07_23');
+gt=data;
+clear data;
 
-%% Load annotations - previous data, already analyzed
-%Subject1, 4 orientations, 2nd try in total, 1st try with ellipse tool
-
-%tic;
-%https://nl.mathworks.com/matlabcentral/fileexchange/33381-jsonlab--a-toolbox-to-encode-decode-json-files
-%data1=loadjson(fullfile(dataPath, 'Veronika_Slices_Subject1_Try2_20160525_0805.json'), 'ShowProgress', 1); 
-%toc;
-%43 seconds
-
-%tic;
-%Get JSON toolbox from http://www.artefact.tk/software/matlab/jsonio/ 
-%data2=jsonread(fullfile(dataPath, 'Veronika_Slices_Subject1_Try2_20160525_0805.json')); 
-%toc;
-%2 seconds. AT least 10 mins for full file, doesn't finish
-
-%%
-%All subjects (Subject batch number 2), 1 orientation - this data has not been analyzed yet! 
-
-
-%https://nl.mathworks.com/matlabcentral/fileexchange/23393--another--json-parser
+%% Load crowdsourcing data for all subjects 
 
 tic;
-s=fileread(fullfile(dataPath, 'Veronika_Slices_Subject2_Try1_20160622_1043.json'));
-data=parse_json(s); 
+s=fileread(fullfile(dataPath, 'Veronika_Slices_Subject2_Try1_20160622_1043.json')); %In the file name, "Subject2" means it is the second batch of subjects (all)
+
+%Parse JSON file (the code is from
+%https://nl.mathworks.com/matlabcentral/fileexchange/23393--another--json-parser)
+data=parse_json(s); %This is a bit slow, takes 2 minutes on Veronika's laptop
 toc;
-%2 minutes! fastest method
 
-%%
-%TODO: some parameters needed later?
-C=get(gca,'colororder');
+%% Initialize parameters
 
-voxelSize = 0.55;  %0.5508 x 0.5508 x 0.6000, how to convert distances in 2D slices back to real-world measurements? 
+%How to convert distances in 2D slices to real-life distances in 3D
+voxelSize = 0.55;  %The real voxel size is 0.5508 x 0.5508 x 0.6000, assuming equal size right now
 imageScaling = 0.1;
-
 scaleFactor = voxelSize*imageScaling;
 
-
-
-%%  For easy analysis, let's collect data in tables
-
+%Arrays to collect data
 gtTable = [];    %Ground truth measurements (Wiyeing and Adria's algorithm)
-
 dataTable = [];  %Details of task 
 annotTable = []; %Measurements of annotation 
+whichAnnotator = '';  %A string to store IDs of the crowd annotators
 
 
+%How many images to display
+showImages = 0;  %If set to 1, the code below will show 100 images, and you need to press the spacebar before going to the next image
+
+
+
+
+%% Go through the crowdsourcing tasks
+
+%How many tasks are there
 numTasks = length(data.project.tasks);
 
-
-whichAnnotator = '';
-
-
-showImages = 0;
-
+%How many tasks to go through
 if showImages == 1
     numTasksToShow = 100;
 else
     numTasksToShow = numTasks;
 end
 
-
+%Go through the tasks
 for i=1:numTasksToShow
-    [i numTasksToShow]
+    fprintf('Task %d of %d\n', i, numTasksToShow);
+    
+    %Extract data of this task
     currentTask = data.project.tasks(i);
-    currentTask = currentTask{1};        %Necessary because of the way parse_json loads the files, otherwise could use data.project.tasks(i) directly
+    currentTask = currentTask{1};        %Necessary because of the way parse_json.m loads the files
     
     fileName = currentTask.frame.original;
     fileParts = strsplit(fileName, '/');
-    origFileName = fileParts{length(fileParts)}; 
+    origFileName = fileParts{length(fileParts)}; %Filename corresponding to the airway image
         
     ids = sscanf(origFileName, 'data(%d).airways(%d).viewpoints(%d)', [1, inf]);
-    subjectID = ids(1);
-    airwayID = ids(2);
-    viewpoint = ids(3);
-        
+    subjectID = ids(1);     %Which subject the airway is from
+    airwayID = ids(2);      %Which airway of that subject is it
+    viewpoint = ids(3);     %Which viewpoint of the airway (in this data, this is always 1)
+            
     
-    
+    %How many crowdsourcing results are there for this task
     numRes = length(currentTask.results);
-    if numRes>0
 
-        %Get original image of that task
+    %If there are any results, (optionally) show them, and measure the annotations
+    if numRes>0
+        %Get original image of this airway
         if showImages == 1
             im = imread([dataPath '\slices_GoLive2\' origFileName]);
         end
-
-        for j=1:numRes
+        
+        %Load the ground truth measurements for this airway
+        whichGT=gt(subjectID).airways(airwayID).gt.id;
+        areaInnerWieying=gt(subjectID).gt(whichGT).inner.global_area;
+        areaOuterWieying=gt(subjectID).gt(whichGT).outer.global_area;
             
+        whichPoint=gt(subjectID).airways(airwayID).gt.airway_point_id;
+        areaInnerAdria = gt(subjectID).airways(airwayID).inner.area(whichPoint);
+        areaOuterAdria = gt(subjectID).airways(airwayID).outer.area(whichPoint);
+                                   
+        vec = gt(subjectID).airways(airwayID).viewpoints.vec;
+
+        %Go through crowdsourcing results
+        for j=1:numRes
+          
+            %Display image and ground truth measurements in the image
             if showImages == 1
                 imshow(im);
-            end
-                                    
-            whichGT=gt(subjectID).airways(airwayID).gt.id;
-            areaInnerWieying=gt(subjectID).gt(whichGT).inner.global_area;
-            areaOuterWieying=gt(subjectID).gt(whichGT).outer.global_area;
-            
-            whichPoint=gt(subjectID).airways(airwayID).gt.airway_point_id;
-            areaInnerAdria = gt(subjectID).airways(airwayID).inner.area(whichPoint);
-            areaOuterAdria = gt(subjectID).airways(airwayID).outer.area(whichPoint);
-            
-
-                       
-            vec = gt(subjectID).airways(airwayID).viewpoints.vec;
-                        
-            if showImages == 1
                 title(sprintf('Wieying %2.1f-%2.1f, Adria %2.1f-%2.1f', areaInnerWieying, areaOuterWieying, areaInnerAdria, areaOuterAdria));
             end
-                        
+            
+            %Go through the annotations within this result (ideally, there
+            %should be two annotations, for the inner and outer airway)
             currentResults = currentTask.results(j);
             currentResults = currentResults{1};
-            
             numAnnot=length(currentResults.annotations);
-            
-                       
-            
-            %Create structures to store information about ellipses, to
-            %use for analysis later
+                        
+            % Measure the ellipses in this image. This function loops through the annotations
             [centerX, centerY, angle, radius1, radius2] = measureEllipses(currentResults.annotations);
   
+            % Draw ellipses. This functions loops through the annotations
             if showImages == 1
-                   ellipse(radius1,radius2,angle,centerX,centerY);    
+                ellipse(radius1,radius2,angle,centerX,centerY);    
             end
-                        
+            
+            % Decide if these annotations are "usable". This is now defined
+            % as two ellipses within each other (for inner and outer way),
+            % but this could be changed
             useAnnotation = classifyAnnnotation(centerX, centerY, angle, radius1, radius2);
             
-            
+            % Record the IDs of the annotators who made the annotations
             if numAnnot > 0
                 currentAnnotation = currentResults.annotations(1);
                 currentAnnotation = currentAnnotation{1};
@@ -147,13 +130,16 @@ for i=1:numTasksToShow
                 whichAnnotator = strvcat(whichAnnotator, 'unknown');
             end
             
-            
-            dataTable = [dataTable; i numAnnot useAnnotation];
+            %Add airway ground truth to the corresponding array
             gtTable = [gtTable; areaInnerWieying, areaOuterWieying, areaInnerAdria, areaOuterAdria];
-           
             
-            %TODO Need to transform length of radii to world coordinates
+            %Add crowdsourcing details to the corresponding array
+            dataTable = [dataTable; i numAnnot useAnnotation];
             
+                       
+            %Measure the ellipses - this currently assumes that voxels are square, which is not the case. 
+            
+            %If there are 2 or more ellipses
             if length(radius1)>1
                 
                 area1=(radius1(1)*scaleFactor) * (radius2(1)*scaleFactor) * pi;
@@ -163,13 +149,14 @@ for i=1:numTasksToShow
                     xlabel(sprintf('Use %d, %2.1f-%2.1f', useAnnotation, area1, area2));
                 end
                 
+                %Add measurements to the corresponding array
                 annotTable = [annotTable; min(area1,area2), max(area1,area2)];
-            
+                
+            %If there is only 1, or 0 ellipses
             else
                 if showImages == 1
                     xlabel(sprintf('Use %d', useAnnotation));
                 end
-                
                 if length(radius1)==1
                       area1=(radius1(1)*scaleFactor) * (radius2(1)*scaleFactor) * pi;
                       annotTable = [annotTable; area1 nan];
@@ -178,7 +165,6 @@ for i=1:numTasksToShow
                 end
             end
             
-                        
             if showImages == 1
                 pause;
             end
@@ -186,6 +172,7 @@ for i=1:numTasksToShow
     end
 end
 
+%Save the results
 save([resultPath 'annotationSummary_allSubjects.mat'], 'gtTable', 'dataTable', 'annotTable');
 
 
