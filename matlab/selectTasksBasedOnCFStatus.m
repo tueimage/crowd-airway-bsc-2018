9e6b12ba476e5ab140b234c231216961835813f5
Eussen <m.j.a.eussen@student.tue.nl>
2018-05-22 14:38:36 +0200

    recent version of CFstatus script, which is still incorrect

--- matlab/selectTasksBasedOnCFStatus.m
+++ matlab/selectTasksBasedOnCFStatus.m
@@ -15,30 +15,67 @@ for x=1:length(subjectIDandStatus)
 end
 
 %% Select the tasks of patients with CF and tasks of patients without CF
-
-% Hier gaat hij de mist in, want meerdere tasks/patient, maar hij pakt nu
-% telkens maar een patient. 
-% 
+%Het totaal van taskNoCF en taskYesCF zou op 2380 moeten uitkomen.
 taskNoCF=[];
-previouspatient=0;
-for i=1:length(noCF)
-    for j=1:numTasks
-        if filterDataTable(j,4)== noCF(i) & previouspatient~=noCF(i)
-            previouspatient=noCF(i);
-            taskNoCF=[taskNoCF; filterDataTable(j,1)];
+previouspatientNo=0;
+
+for i=1:length(filterDataTable)
+    patientNoCF=[]; 
+    for j=1:length(noCF)   
+        if filterDataTable(i,4)== noCF(j) && previouspatientNo ~=noCF(j)
+            previouspatientNo=noCF(j);
+            patientNoCF=[patientNoCF; filterDataTable(i,1)];
         end
     end
+    taskNoCF=[taskNoCF; patientNoCF];
 end
 
 taskYesCF=[];
-previouspatient=0;
-for k=1:length(yesCF)
-    for l=1:numTasks
-        if filterDataTable(l,4)== yesCF(k) & previouspatient~=yesCF(k)
-            previouspatient=yesCF(k);
-            taskYesCF=[taskYesCF; filterDataTable(l,1)];
+previouspatientYes=0;
+
+for n=1:length(filterDataTable)
+    patientYesCF=[]; 
+    for m=1:length(noCF)   
+        if filterDataTable(n,4)== yesCF(m) && previouspatientYes ~=yesCF(m)
+            previouspatientYes=yesCF(m);
+            patientYesCF=[patientYesCF; filterDataTable(n,1)];
         end
     end
+    taskYesCF=[taskYesCF; patientYesCF];
 end
 
+
+%% Select the tasks of patients with CF and tasks of patients without CF
+
+% % Hier gaat hij de mist in, want meerdere tasks/patient, maar hij pakt nu
+% % telkens maar een patient. 
+% 
+% taskNoCF=[];
+% previouspatient=0;
+% 
+% for i=1:length(noCF)
+%     patientNoCF=[]; 
+%     for j=1:length(filterDataTable)       
+%         if filterDataTable(j,4)== noCF(i) && noCF(i)~=previouspatient
+%             previouspatient=noCF(i);
+%             patientNoCF=[patientNoCF; filterDataTable(j,1)];
+%         end
+%     end
+%     taskNoCF=[taskNoCF; patientNoCF];
+% end
+% 
+% taskYesCF=[];
+% previouspatient=0;
+% for k=1:length(yesCF)
+%     for l=1:length(filterDataTable)
+%         if filterDataTable(l,4)== yesCF(k) && previouspatient~=yesCF(k)
+%             previouspatient=yesCF(k);
+%             taskYesCF=[taskYesCF; filterDataTable(l,1)];
+%         end
+%     end
+% end
+% 
+% 
+
+%% dit weer linken aan indexResultsTask
     
