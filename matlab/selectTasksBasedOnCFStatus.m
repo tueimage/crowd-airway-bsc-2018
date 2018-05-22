9fc2214dbc78372fd817f4cd34642bc6879b915e
Veronika <v.cheplygina@tue.nl>
2018-05-22 17:55:08 +0200

    Another solution for which tasks with CF or not

--- matlab/selectTasksBasedOnCFStatus.m
+++ matlab/selectTasksBasedOnCFStatus.m
@@ -1,7 +1,31 @@
 [dataPath slicePath resultPath] = getPath;
 load([resultPath 'subjects_status.mat'], 'subjectIDandStatus');
 load([resultPath 'filterUsefulResults.mat'], 'filterGtTable', 'filterDataTable', 'filterAnnotTable');
-numTasks=2380;
+numTasks=2380; 
+
+%% VC trying to find the task IDs of subjects belonging to CF or not
+load([resultPath 'annotationSummary_allSubjects.mat'], 'gtTable', 'dataTable', 'annotTable', 'gtTablePerTask');
+
+%How many tasks in total
+uniqueTaskNumbers = unique(dataTable(:,1));
+numTasks = length(uniqueTaskNumbers);
+
+%Which subjects have CF or not
+subjectsNoCF = subjectIDandStatus(find(subjectIDandStatus(:,2) == 1),1);
+subjectsYesCF = subjectIDandStatus(find(subjectIDandStatus(:,2) == 2),1);
+
+%Which tasks have subject IDs, that belong to subjects without CF
+indexTaskNoCF = find(ismember(dataTable(:,4), subjectsNoCF));
+taskIDNoCF = unique(dataTable(indexTaskNoCF,1))
+
+%Which tasks have subject IDs, that belong to subjects with CF
+indexTaskYesCF = find(ismember(dataTable(:,4), subjectsYesCF));
+taskIDYesCF = unique(dataTable(indexTaskYesCF,1))
+
+
+%Sanity check
+assert(numTasks == length(taskIDYesCF) + length(taskIDNoCF), "Numbers of tasks do not add up"); %If you do not see this error message then things are fine
+
 
 %% select subjects with or without CF
 noCF=[];
@@ -11,6 +35,8 @@ for x=1:length(subjectIDandStatus)
         noCF=[noCF; subjectIDandStatus(x,1)];
     elseif subjectIDandStatus(x,2)==2
         yesCF=[yesCF; subjectIDandStatus(x,1)];
+    else
+        error('This should not happen');
     end
 end
 
